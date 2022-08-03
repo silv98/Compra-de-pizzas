55b216368f31d4115770e679d6f96762a5050706
Evelyn <evelynnkarinas@gmail.com>
2022-08-03 14:16:55 -0300

    adicionando imagens

--- pizza.js
+++ pizza.js
@@ -2,7 +2,7 @@ let pizzaJson = [
   {
     id: 1,
     name: "Mussarela",
-    img: "../imagens/pizza.png",
+    img: "../Imagens/pizza.png",
     price: 20.19,
     sizes: ["100g", "530g", "860g"],
     description:
@@ -11,7 +11,7 @@ let pizzaJson = [
   {
     id: 2,
     name: "Calabresa",
-    img: "../imagens/pizza2.png",
+    img: "../Imagens/pizza2.png",
     price: 18.0,
     sizes: ["320g", "530g", "860g"],
     description:
@@ -20,7 +20,7 @@ let pizzaJson = [
   {
     id: 3,
     name: "Quatro Queijos",
-    img: "../imagens/pizza3.png",
+    img: "../Imagens/pizza3.png",
     price: 17.45,
     sizes: ["320g", "530g", "860g"],
     description:
@@ -29,7 +29,7 @@ let pizzaJson = [
   {
     id: 4,
     name: "Americana",
-    img: "../imagens/pizza4.png",
+    img: "../Imagens/pizza4.png",
     price: 19.77,
     sizes: ["320g", "530g", "860g"],
     description:
@@ -38,7 +38,7 @@ let pizzaJson = [
   {
     id: 5,
     name: "Sorvete",
-    img: "../imagens/pizza5.png",
+    img: "../Imagens/pizza5.png",
     price: 21.43,
     sizes: ["320g", "530g", "860g"],
     description:
@@ -47,7 +47,7 @@ let pizzaJson = [
   {
     id: 6,
     name: "Moda da Casa",
-    img: "../imagens/pizza6.png",
+    img: "../Imagens/pizza6.png",
     price: 18.55,
     sizes: ["320g", "530g", "860g"],
     description:
@@ -56,7 +56,7 @@ let pizzaJson = [
   {
     id: 7,
     name: "Chocolate",
-    img: "../imagens/pizza7.png",
+    img: "../Imagens/pizza7.png",
     price: 22.36,
     sizes: ["320g", "530g", "860g"],
     description:
